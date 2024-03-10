0e0dd2ebcbab8612b36eebf19871dd70637a942a
yana <yanamyh2005@gmail.com>
2024-03-10 20:48:38 +0200

    1

---
+++ Test/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Test
+{
+    public class Person
+    { 
+        public string name { get; set; }
+        public int age { get; set; }
+        public Gender sex { get; set; }
+
+        public Person(string name, int age, Gender sex)
+        {
+            this.name = name;
+            this.age = age;
+            this.sex = sex;
+        }
+
+        public void ShowDta()
+        {
+            Console.WriteLine($"{name} {age} {sex}");
+        }
+    }
+}
